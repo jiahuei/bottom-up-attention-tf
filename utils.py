054f566d8deaa75c308cf2677f5f1fd68aa55093
jiahuei <tanjiahuei@gmail.com>
2020-04-11 17:41:24 +0800

    add missing methods to utils.py
    reference:
    https://github.com/hengyuan-hu/bottom-up-attention-vqa/blob/8e68d15573695feaa42b752a12b357c11315efd5/utils.py

--- utils.py
+++ utils.py
@@ -1,17 +1,50 @@
-from __future__ import print_function
-
+import errno
 import os
 import numpy as np
+from PIL import Image
 
 EPS = 1e-7
+
 
 def assert_eq(real, expected):
     assert real == expected, '%s (true) vs %s (expected)' % (real, expected)
 
 
 def assert_array_eq(real, expected):
-    assert (np.abs(real-expected) < EPS).all(), \
+    assert (np.abs(real - expected) < EPS).all(), \
         '%s (true) vs %s (expected)' % (real, expected)
+
+
+def load_folder(folder, suffix):
+    imgs = []
+    for f in sorted(os.listdir(folder)):
+        if f.endswith(suffix):
+            imgs.append(os.path.join(folder, f))
+    return imgs
+
+
+def load_imageid(folder):
+    images = load_folder(folder, 'jpg')
+    img_ids = set()
+    for img in images:
+        img_id = int(img.split('/')[-1].split('.')[0].split('_')[-1])
+        img_ids.add(img_id)
+    return img_ids
+
+
+def pil_loader(path):
+    with open(path, 'rb') as f:
+        with Image.open(f) as img:
+            return img.convert('RGB')
+
+
+def create_dir(path):
+    if not os.path.exists(path):
+        try:
+            os.makedirs(path)
+        except OSError as exc:
+            if exc.errno != errno.EEXIST:
+                raise
 
 
 def convert_entries(entries):
